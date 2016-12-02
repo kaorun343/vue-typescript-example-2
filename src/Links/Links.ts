d4d6fe45c0db8a7926d508b87613fceffc4aaeeb
kaorun343 <kaoru.h65536@live.jp>
2016-12-02 21:18:03 +0900

    修正

--- src/Links/Links.ts
+++ src/Links/Links.ts
@@ -7,6 +7,7 @@ export default class Links extends Vue {
   links = [
     { title: 'vue.js', href: 'https://jp.vuejs.org' },
     { title: 'TypeScript - JavaScript that scales.', href: 'http://www.typescriptlang.org' },
+    { title: 'Bootstrap · The most popular HTML, CSS, and JS framework in the world.', href: 'http://v4-alpha.getbootstrap.com' },
     { title: 'Vue.js Advent Calendar 2016 - Qiita', href: 'http://qiita.com/advent-calendar/2016/vue' },
     { title: 'TVアニメ「ご注文はうさぎですか？？」公式サイト', href: 'http://www.gochiusa.com' },
     { title: 'ご注文はうさぎですか? - Wikipedia', href: 'https://ja.wikipedia.org/wiki/ご注文はうさぎですか%3F' }
--- src/navbar/navbar.ts
+++ src/navbar/navbar.ts
@@ -3,4 +3,10 @@ import Component from 'vue-class-component'
 import * as template from './navbar.template.html'
 
 @Component(template({}))
-export default class Navbar extends Vue {}
+export default class Navbar extends Vue {
+  links = [
+    { title: 'ホーム', name: 'home' },
+    { title: 'リンク一覧', name: 'links' },
+    { title: '設定', name: 'settings' }
+  ]
+}
